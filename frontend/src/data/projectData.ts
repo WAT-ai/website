095aa1b3d9175b807045d1330a1f537ad9fda1c8
Ryan Xiang <xiangryan11@gmail.com>
2024-08-27 19:37:34 -0400

    Move projects page to react

---
+++ frontend/src/data/projectData.ts
@@ -0,0 +1,61 @@
+// src/data/projectData.ts
+
+export const ProjectData = [
+    // add more here...
+  ];
+  
+  export const PastProjects = [
+    {
+      title: "PlayFitt Recommender System",
+      tpm: "Ben Bates, Richard Wills",
+      coreMembers: "Will Erf, Kristof Sochan, Nolan White-Roy, Ross Cleary",
+      description:
+        "Our objective is to help people be more active! We developed a recommender system for the PlayFitt fitness app using a contextual bandit algorithm. This takes into account information about the users as context, and makes decisions about rep counts and reward values to suggest. Notably, this approach enables online learning.",
+      collaboration: "Intellisports",
+      collaborationLogo: "intellisport.png",
+      collaborationLink: "https://www.playfitt.ca",
+    },
+    {
+      title: "Solar Photovoltaic Output Prediction",
+      tpm: "Areel Khan, Carter Demars",
+      coreMembers: "Anupra Chandran, Bill Bai, Mihir Gupta, Raihan Abdul Vaheed",
+      description:
+        "We set out to develop deep learning models that predict solar photovoltaic output. These forecasts on solar energy production can drive better energy decisions, massively reducing carbon emmissions produced by power grids.",
+      collaboration: "Open Climate Fix",
+      collaborationLogo: "ocf.png",
+      collaborationLink: "https://openclimatefix.org",
+    },
+    {
+      title: "Prostate Cancer Prediction With Correlated Diffusion Imaging",
+      tpm: "Hargun Mujral, Jarett Dewbury",
+      coreMembers: "Adam Lam, Alex Shao, Michelle Watson, Praajna Baragur",
+      description:
+        "The application of machine learning to medical images has led to impressive advancement in cancer diagnostics. Our objective is to develop a baseline of deep learning models to detect the presence of prostate cancer within a novel Correlated Diffusion Imaging (CDI) dataset.",
+      collaboration: "Dr. Alexander Wong & Hayden Gunraj",
+      collaborationLogo: "vip-lab.png",
+      collaborationLink: "https://uwaterloo.ca/vision-image-processing-lab/",
+    },
+    {
+      title: "Deep Learning Framework Comparison",
+      tpm: "Anusha Raisinghani, Trevor Yu",
+      coreMembers: "Adish Shah, Ethan Gabriel, Musaab Siddiqui, Urban Pistek, Yask Pokra",
+      description:
+        "The goal of our project is to benchmark performance of several deep learning frameworks, including TensorFlow, PyTorch, Jax, MxNet, Flux.jl, and KNet.jl. We build models from scratch in each framework and test them on common datasets.",
+    },
+    {
+      title: "Reinforcement Learning Chess Engine",
+      tpm: "Amya Singhal, Thomas Fortin",
+      coreMembers: "Alex He, Alexander Hutchinson, Ishaan Patel, Shivam Jindal",
+      description:
+        "We are developing an artificial intelligence chess engine based on the work of Deep Mind on their chess engine━Alpha-Zero. We are also iteratively testing and modifying the model to improve performance on hardware with much more limited processing power than what was available to Deep Mind when creating Alpha-Zero.",
+    },
+    {
+      title: "Stable Diffused Adversarial Attacks",
+      tpm: "Andy Wu, Dhrumil Patel, Rayaq Siddiqui",
+      coreMembers: "Eric Sheen, Harsh Patel, Iliad Shaghaghi, Nicole Jin, Ryan Shen",
+      description:
+        "We are exploring pre-trained computer vision models (i.e. MobileNet_v1) with the goal to exploit vulnerabilities through various adversarial attacks. Our work aims to develop an architecture that automates adversarial attack image generation in model misclassification.",
+    },
+    // add more here...
+  ];
+  
--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -1,5 +1,100 @@
-import React from 'react';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import { ProjectData, PastProjects } from "../data/projectData";
+import ProjectCard from "../components/ProjectCard";
+import { keyframes } from "@mui/system";
 
-const Projects: React.FC = () => <div>Projects Page</div>;
+// keyframes for the slideshow animation
+const moveSlideshow = keyframes`
+  100% { 
+    transform: translateX(-49.23333%);  
+  }
+`;
+
+const Projects: React.FC = () => {
+  const [showPastProjects, setShowPastProjects] = useState(false);
+
+  return (
+    <Box sx={{ padding: "20px" }}>
+      {/* Current Projects Section */}
+      <ProjectSection title="Current Projects" projects={ProjectData} />
+
+      {/* Toggle Past Projects Section */}
+      <Box sx={{ textAlign: "center", mb: 5 }}>
+        <Typography variant="h4" sx={{ textAlign: "center", mt: 5, mb: 2 }}>
+          Past Projects
+        </Typography>
+        <IconButton
+          onClick={() => setShowPastProjects(!showPastProjects)}
+          sx={{ color: "#FFF" }}
+          aria-label="toggle past projects"
+        >
+          {showPastProjects ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        </IconButton>
+        {showPastProjects && (
+          <Box sx={{ mt: 3 }}>
+            <ProjectSection title="" projects={PastProjects} alignTitle="center" />
+          </Box>
+        )}
+      </Box>
+
+      {/* Partners Section with Auto Scroller */}
+      <Box sx={{ marginTop: "150px", marginBottom: "-50px" }}>
+        <Box sx={{ marginTop: "50px" }} className="slide"></Box>
+        <Box sx={{ marginTop: "-120px" }}>
+          <Typography variant="h4" sx={{ textAlign: "center", mb: 2 }}>
+            Our Partners
+          </Typography>
+          <Box className="tech-slideshow" sx={{
+            height: "200px",
+            width: "100%",
+            position: "relative",
+            overflow: "hidden",
+          }}>
+            <Box sx={{
+              marginTop: "-50px",
+              width: "3184px",
+              backgroundImage: `url(${require('../assets/slider3.png')})`,
+              position: "absolute",
+              top: "0",
+              left: "0",
+              zIndex: "0",
+              height: "100%",
+              transform: "translate3d(0, 0, 0)",
+              animation: `${moveSlideshow} 22s linear infinite`,
+            }} className="mover-1"></Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+// Project section component
+const ProjectSection = ({ title, projects, alignTitle = "center" }: { title: string; projects: any[]; alignTitle?: "center" | "left"; }) => (
+  <Box sx={{ mt: 5, mb: 5 }}>
+    <Typography variant="h4" sx={{ textAlign: alignTitle, mb: 5 }}>
+      {title}
+    </Typography>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        paddingLeft: "10px",
+        paddingRight: "10px",
+        mt: "50px",
+      }}
+    >
+      {projects.map((project, index) => (
+        <ProjectCard key={index} {...project} />
+      ))}
+    </Box>
+  </Box>
+);
 
 export default Projects;
