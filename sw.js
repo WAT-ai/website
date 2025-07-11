429485982146fce207a1aab4c2b13a0339439753
adi <90326965+adithayyil@users.noreply.github.com>
2025-07-10 21:36:23 -0400

    Updates

---
+++ sw.js
@@ -0,0 +1,77 @@
+// Service Worker for aggressive caching and performance
+const CACHE_NAME = 'watai-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/static/js/main.js',
+  '/static/css/main.css',
+  '/manifest.json',
+];
+
+// Install event - cache essential resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(STATIC_ASSETS);
+      })
+  );
+});
+
+// Fetch event - serve from cache when possible
+self.addEventListener('fetch', (event) => {
+  if (event.request.destination === 'image') {
+    // Cache images aggressively
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME)
+                  .then((cache) => {
+                    cache.put(event.request, responseClone);
+                  });
+              }
+              return response;
+            });
+        })
+    );
+  } else {
+    // Network first for other resources
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          if (response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseClone);
+              });
+          }
+          return response;
+        })
+        .catch(() => {
+          return caches.match(event.request);
+        })
+    );
+  }
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
